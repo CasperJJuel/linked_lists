a16036fd73afe7fcc9ac3e226b5d84c9ab513a96
Casper Juel <Casper-juel@hotmail.com>
2024-01-24 19:59:45 +0100

    Initiate product and adding methods needed

---
+++ main.rb
@@ -0,0 +1,42 @@
+class LinkedList
+  #Represents the full list
+
+  def append(value)
+  end
+
+  def prepend(value)
+  end
+
+  def size
+  end
+
+  def head
+  end
+
+  def tail
+  end
+
+  def at(index)
+  end
+
+  def pop
+  end
+
+  def contains(value)
+  end
+
+  def find(value)
+  end
+
+  def to_s
+  end
+end
+
+class Node
+  @value = nil
+  #@next_node = nil
+
+  def value
+    @value
+  end
+end
